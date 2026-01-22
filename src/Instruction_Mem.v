4603cf35c4e7a350542d30a5bf93528e1cfa8ac9
VARJULA BALAKRISHNA <varjulabalakrishna2002@gmail.com>
2026-01-22 16:06:13 +0530

    Update Instruction_Mem.v

--- src/Instruction_Mem.v
+++ src/Instruction_Mem.v
@@ -10,7 +10,7 @@ reg [31:0] Instr_Mem [1023:0];
 assign RD = (rst == 1'b1) ? {32{1'b0}} : Instr_Mem[A[31:2]];
 
 initial begin
-    $readmemh("C:/Kittu/B.Tech_GITAM/Sem_32/DDFPGA/LAB/project_6/IntrucMem.hex", Instr_Mem);
+    $readmemh("../IntrucMem.hex", Instr_Mem);  // The path should be complete, which I have not added here.
 end
 
 endmodule
