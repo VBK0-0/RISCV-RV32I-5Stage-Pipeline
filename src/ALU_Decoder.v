2c38b5647f06973390c1de45b3d3240149fcf2d9
VARJULA BALAKRISHNA <varjulabalakrishna2002@gmail.com>
2026-01-22 12:55:54 +0530

    Update ALU_Decoder.v

--- src/ALU_Decoder.v
+++ src/ALU_Decoder.v
@@ -7,11 +7,11 @@ input [6:0] funct7,Op;
 input [2:0] funct3;
 output [2:0] ALU_Ctrl;
 
-assign ALU_Ctrl = (ALU_Op == 2'b00) ? 3'b000 :
-                  (ALU_Op == 2'b01) ? 3'b001 :
-                  (ALU_Op == 2'b10) & (funct3 == 3'b010) ? 3'b101 : 
-                  (ALU_Op == 2'b10) & (funct3 == 3'b110) ? 3'b011 :
-                  (ALU_Op == 2'b10) & (funct3 == 3'b111) ? 3'b010 :
-                  (ALU_Op == 2'b10) & (funct3 == 3'b000) & ({Op[5],funct7[5]} == 2'b11) ? 3'b001 : 3'b000;
+assign ALU_Ctrl = (ALU_Op == 2'b00) ? 3'b000 :                                      //  ADD
+      (ALU_Op == 2'b01) ? 3'b001 :                                      // SUB (for BEQ, BNE)
+      (ALU_Op == 2'b10) & (funct3 == 3'b010) ? 3'b101 :                 // SLT
+      (ALU_Op == 2'b10) & (funct3 == 3'b110) ? 3'b011 :                 // OR
+      (ALU_Op == 2'b10) & (funct3 == 3'b111) ? 3'b010 :                 // AND
+      (ALU_Op == 2'b10) & (funct3 == 3'b000) & ({Op[5],funct7[5]} == 2'b11) ? 3'b001 : 3'b000; // SUB
                   
 endmodule
